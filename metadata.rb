30df2d5dc5e2d959f8bd6c18b6099d92407cc8f5
Konstantin Gredeskoul & Richard Millan <wanelo-pair@wanelo.com>
2013-04-23 13:46:26 -0700

    Add maintenance cookbook to restart chef-client nightly (workaround for memory balooning/leak)

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,13 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Manages aspects of only chef-client"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "2.1.4"
+version           "2.1.5"
 recipe            "chef-client", "Includes the service recipe by default."
 recipe            "chef-client::config", "Configures the client.rb from a template."
 recipe            "chef-client::service", "Sets up a client daemon to run periodically"
 recipe            "chef-client::delete_validation", "Deletes validation.pem after client registers"
 recipe            "chef-client::cron", "Runs chef-client as a cron job rather than as a service"
+recipe            "chef-client::maintenance", "Restart chef-client nightly because it leaks"
 
 %w{ ubuntu debian redhat centos fedora oracle suse freebsd openbsd mac_os_x mac_os_x_server windows smartos }.each do |os|
   supports os
